3f3d1a30ce29cf55366214e12f03ba6e78b7b56d
bycdiaz <cdiaz86@gmail.com>
2019-08-22 20:52:07 -0400

    Adds solution addressing multiple word strings.

--- substrings.rb
+++ substrings.rb
@@ -5,27 +5,10 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 # initial solution
-def substrings(word,dictionary)
-  word = word.downcase.gsub(/[^a-z0-9\s]/i, '')
-  repeats = Hash.new 0
-  dictionary.each { |entry|
-    if word.include?(entry)
-      # puts "#{entry} was found inside of #{word}!"
-      repeats[entry] += 1
-    end
-  }
-  repeats
-end
-
-# addressing string with many words
 # def substrings(word,dictionary)
 #   word = word.downcase.gsub(/[^a-z0-9\s]/i, '')
 #   repeats = Hash.new 0
 #   dictionary.each { |entry|
-#     word.downcase.split(" ").each { |word|
-     
-#     }
-
 #     if word.include?(entry)
 #       # puts "#{entry} was found inside of #{word}!"
 #       repeats[entry] += 1
@@ -34,10 +17,23 @@ end
 #   repeats
 # end
 
-
-p substrings("below", dictionary)
+# p substrings("below", dictionary)
 # { "below" => 1, "low" => 1 }
 
-# p substrings("Howdy partner, sit down! How's it going?", dictionary)
-{ "down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1 }
+# addressing string with many words
+def substrings(word,dictionary)
+  word = word.downcase.gsub(/[^a-z0-9\s]/i, '')
+  repeats = Hash.new 0
+  dictionary.each { |entry|
+    word.split(" ").each { |word|
+      if word.include?(entry)
+        # puts "#{entry} was found inside of #{word}!"
+        repeats[entry] += 1
+      end
+    }
+  }
+  repeats
+end
 
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
+# { "down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1 }
